a498c19d2dbbb610f04d816ad895d9fee5ddc72c
Rauf0495 <raufzm@code.edu.az>
2022-04-10 00:07:25 +0400

    Add project files.

---
+++ Task4.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-1000 qeder CUT ederlerin icerisinde
+            //reqemleri cemi 5 ile 7 arasinda olan en boyuk eded hansidir?
+
+
+            int num = 0;
+
+            int sum = 0;
+            
+            for (int i = 1; i <= 1000; i++)
+            {               
+
+                if (i%2==0)
+                {
+                    int a = i;
+                    
+                    while (a>0)
+                    {                        
+                        sum = sum + a % 10;
+
+                        a = a / 10;                       
+                    }
+
+                    if (sum == 6)
+                    {
+                        num = i;
+                    }
+
+
+                }
+
+                sum = 0;
+            }
+
+            Console.WriteLine(num);
+
+
+
+
+        }
+    }
+}
---
+++ Task4.2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Task4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-1000 qeder ederlerin icerisinden ele ededleri cap etki :
+            //hem polindrom olsun,
+            //hem ededin reqemleri daxilinde 3 olmasin
+            //hem ededin reqemleri cemi 10 dan boyuk olsun
+
+            int num =232;           
+            int num1 = num;           
+            int num2 = num;           
+            int rev =0;
+            int rem = 0;           
+            
+            bool polState=false;
+            bool divide3=false;
+            bool sum10=false;
+
+            while (num>0)
+            {
+                rem = rem + num % 10;
+
+                if (rem<10)
+                {
+                    sum10 = true;
+                }
+
+                num /= 10;               
+
+            }
+
+            while (num1 > 0)
+            {
+                rev = (rev*10)+ (num1 % 10);
+                num1 /= 10;
+            }
+
+            if (rem== rev)
+            {
+                polState = true;
+            }
+
+            while (num2>0)
+            {
+                int qaliq = num2 % 10;
+
+                if (qaliq==3)
+                {
+                    divide3 = true;
+                    break;
+                }
+                num2/=10;
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
